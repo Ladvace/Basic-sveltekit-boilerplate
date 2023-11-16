ffc147aca781cab33f894c9e01064b9ecb2a3fa9
Gianmarco <cavallogianmarco@gmail.com>
2023-11-16 16:54:52 +0100

    removed unused stuff

--- uno.config.ts
+++ uno.config.ts
@@ -5,11 +5,29 @@ import presetUno from '@unocss/preset-uno';
 export default defineConfig({
 	presets: [
 		presetUno(),
-		presetWebFonts({}),
+		presetWebFonts({ fonts: { serif: 'Bespoke Serif' }, provider: 'fontshare' }),
 		presetIcons({
 			collections: {
-				hero: () => import('@iconify-json/heroicons/icons.json').then((i) => i.default)
+				hero: () => import('@iconify/json/json/heroicons.json').then((i) => i.default)
 			}
 		})
-	]
+	],
+	theme: {
+		colors: {
+			neutral: {
+				'50': '#E0E0E8',
+				'100': '#B8B8C2',
+				'200': '#9090A0',
+				'300': '#686880',
+				'400': '#505068',
+				'500': '#383850',
+				'600': '#303040',
+				'700': '#282830',
+				'800': '#202028',
+				'900': '#181820',
+				'950': '#101018'
+			},
+			primary: '#D81E5B'
+		}
+	}
 });
